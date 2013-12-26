d8543695d5418ae6a0c317be48c2fc33e3bbee48
Adam Hunter <adamhunter@me.com>
2013-12-26 08:09:00 -0500

    remove silly code

--- lib/rrod/model/form_helper.rb
+++ lib/rrod/model/form_helper.rb
@@ -1,11 +1,6 @@
 module Rrod
   module Model
     module FormHelper
-      def method_missing(method_id, *args, &block)
-        puts caller.binding.class
-      end
     end
-
-    include FormHelper
   end
 end
