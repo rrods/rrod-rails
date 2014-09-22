70421fee834872c4d70f8a38b3df91a3e00b7806
Justin Baker <bakermoto@gmail.com>
2014-09-22 09:51:26 -0400

    Multiple nodes

--- lib/rrod/engine.rb
+++ lib/rrod/engine.rb
@@ -5,7 +5,7 @@ class Rrod::Engine < ::Rails::Engine
   initializer "rrod.rails.configuration" do |app|
     begin
       nodes = YAML.load_file(Rails.root.join('config', 'rrod.yml'))[Rails.env]
-      Rrod.configuration.nodes = nodes 
+      Rrod.configuration.nodes = nodes.is_a?(Hash) ? nodes.fetch(:nodes) : nodes 
     rescue Errno::ENOENT => _e
       message = "Missing config/rrod.yml file please run `rails generate rrod:install`"
       Rails.logger.warn message
