e13223913d4113370e58132fc83cfe8f6108aa1c
Adam Hunter <adamhunter@me.com>
2015-06-11 14:37:57 -0400

    erb config files

--- lib/rrod/engine.rb
+++ lib/rrod/engine.rb
@@ -1,10 +1,11 @@
 require 'yaml'
+require 'erb'
 
 class Rrod::Engine < ::Rails::Engine
 
   initializer "rrod.rails.configuration" do |app|
     begin
-      nodes = YAML.load_file(Rails.root.join('config', 'rrod.yml'))[Rails.env]
+      nodes = YAML.load_file(ERB.new(Rails.root.join('config', 'rrod.yml')).result)[Rails.env]
       Rrod.configuration.nodes = nodes 
     rescue Errno::ENOENT => _e
       message = "Missing config/rrod.yml file please run `rails generate rrod:install`"
